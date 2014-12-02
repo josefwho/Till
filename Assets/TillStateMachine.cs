5ea3c5c667b1407e2553e66254330d71f862cc63
Felix Bohatsch <felix@brokenrul.es>
2014-12-02 13:29:23 +0100

    StateMachine is in

---
+++ Assets/TillStateMachine.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TillStateMachine : MonoBehaviour {
+
+	private FSMSystem machine;
+	
+	public void SetTransition(Transition t) { machine.PerformTransition(t); }
+
+	// Use this for initialization
+	void Start () 
+	{
+		IdleState idle = new IdleState();
+		idle.AddTransition (Transition.ItemGrabbed, StateID.Drag);
+
+		DragState drag = new DragState ();
+		drag.AddTransition (Transition.ItemAtScanner, StateID.Scan);
+
+		machine = new FSMSystem();
+		machine.AddState (idle);
+		machine.AddState (drag);
+
+//		machine
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class IdleState : FSMState
+{
+	public IdleState()
+	{
+		stateID = StateID.Idle;
+	}
+
+	public override void Reason(GameObject owner)
+	{
+		//todo: when item is grabbed transition to next state
+		if (Event has happened) 
+		{
+			owner.GetComponent<TillStateMachine>().SetTransition(Transition.ItemGrabbed);
+		}
+	}
+
+	public override void Act(GameObject owner)
+	{
+				//do nothing
+	}
+}
+
+public class DragState : FSMState
+{
+	public DragState() {
+		stateID = StateID.Drag;
+	}
+
+	public override void Reason(GameObject owner)
+	{
+		//todo: when item at scanner transition to next state
+	}
+	
+	public override void Act(GameObject owner)
+	{
+		//TODO: count how many objects have hit the floor
+	}
+}
+
+
