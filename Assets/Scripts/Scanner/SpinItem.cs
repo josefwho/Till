9252d631fb4480a5b010730ab23dcd51e4038c43
Felix Bohatsch <felix@brokenrul.es>
2014-12-12 13:03:50 +0100

    only allow flicks for spinning item

--- Assets/Scripts/Scanner/SpinItem.cs
+++ Assets/Scripts/Scanner/SpinItem.cs
@@ -5,6 +5,9 @@ public class SpinItem : MonoBehaviour {
 
 	public Vector3 mouseStartPosition;
 	public float speed = 2.0f;
+	public float maxFlickDuration = 0.3f;
+
+	private float flickStartTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +17,7 @@ public class SpinItem : MonoBehaviour {
 	void OnFirstGlobalMouseDown()
 	{
 		mouseStartPosition = Input.mousePosition;
+		flickStartTime = Time.time;
 	}
 
 	void Update()
@@ -33,7 +37,10 @@ public class SpinItem : MonoBehaviour {
 		Vector3 drag = Input.mousePosition - mouseStartPosition;
 		transform.parent.gameObject.rigidbody.AddTorque (0, 0, -drag.x * speed);
 		transform.parent.gameObject.rigidbody.AddTorque (drag.y*speed, 0, 0);
-
+		
+		
+		if (Time.time - flickStartTime > maxFlickDuration)
+						OnFirstGlobalMouseUp ();
 	}
 
 	void OnFirstGlobalMouseUp()
