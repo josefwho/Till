087b176427fe1aeaed92e8f57f0f000e8f663881
Felix Bohatsch <felix@brokenrul.es>
2014-12-31 14:58:08 +0100

    belt parts are now moved collectively by ConveyorBelt script

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -7,9 +7,12 @@ public class ConveyorBelt : MonoBehaviour {
 	public float length;
 	public GameObject partPrefab;
 
+	private ArrayList parts;
+
 	// Use this for initialization
 	void Start () {
-//		GameObject item = Instantiate(prefab, pos, Quaternion.identity ) as GameObject;
+
+		parts = new ArrayList ();
 
 		float partLength = partPrefab.transform.localScale.x;
 		int partsNeeded = Mathf.CeilToInt(length / partLength);
@@ -19,13 +22,18 @@ public class ConveyorBelt : MonoBehaviour {
 
 		for (int i = 0; i < partsNeeded; i++) {
 			GameObject part = Instantiate(partPrefab, pos, Quaternion.identity ) as GameObject;
+			parts.Add(part);
 			pos += offset;
 				}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (Input.GetKey (KeyCode.Space))
-//			transform.Translate(speed * Time.deltaTime, 0, 0);
+		if (Input.GetKey (KeyCode.Space))
+		{
+			for (int i = 0; i < parts.Count; i++) {
+				((GameObject)parts[i]).transform.Translate(speed * Time.deltaTime, 0, 0);
+			}
+		}
 	}
 }
