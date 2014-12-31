f08a4ae41c78b2740fbf7570ed711471ea3941c6
Felix Bohatsch <felix@brokenrul.es>
2014-12-31 15:27:03 +0100

    turned on debug collision
    belt doesn’t work perfectly, but good enough for now

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -8,32 +8,60 @@ public class ConveyorBelt : MonoBehaviour {
 	public GameObject partPrefab;
 
 	private ArrayList parts;
+	private float newPartThreshold;
+	private float distanceSinceLastNewPart;
+	private float removeAtXPos;
+
 
 	// Use this for initialization
 	void Start () {
 
 		parts = new ArrayList ();
 
-		float partLength = partPrefab.transform.localScale.x;
+		float partLength = partPrefab.transform.localScale.x * 0.8f;
 		int partsNeeded = Mathf.CeilToInt(length / partLength);
 
 		Vector3 pos = transform.position;
 		Vector3 offset = new Vector3 (partLength, 0, 0);
+
+		removeAtXPos = pos.x + partsNeeded * partLength;
 
 		for (int i = 0; i < partsNeeded; i++) {
 			GameObject part = Instantiate(partPrefab, pos, Quaternion.identity ) as GameObject;
 			parts.Add(part);
 			pos += offset;
 				}
+		
+		distanceSinceLastNewPart = 0.0f;
+
+		newPartThreshold = partLength ;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.Space))
 		{
-			for (int i = 0; i < parts.Count; i++) {
-				((GameObject)parts[i]).transform.Translate(speed * Time.deltaTime, 0, 0);
+			if(distanceSinceLastNewPart > newPartThreshold)
+			{
+				GameObject part = Instantiate(partPrefab, transform.position, Quaternion.identity ) as GameObject;
+				parts.Add(part);
+				distanceSinceLastNewPart = 0.0f;
 			}
+			
+			float offset = speed * Time.deltaTime;
+
+			foreach(GameObject p in parts)
+			{
+				p.transform.Translate(offset, 0, 0);
+
+				if(p.transform.position.x > removeAtXPos)
+				{
+					parts.Remove(p);
+					GameObject.Destroy(p);
+				}
+			}
+
+			distanceSinceLastNewPart += offset;
 		}
 	}
 }
