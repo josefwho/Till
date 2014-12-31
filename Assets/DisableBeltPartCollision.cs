baaf458133affd5e76875ac6fd931b5931bb5614
Felix Bohatsch <felix@brokenrul.es>
2014-12-31 15:26:40 +0100

    added component that disables collision with belt

---
+++ Assets/DisableBeltPartCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableBeltPartCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "ConveyorBelt") 
+		{
+			other.gameObject.layer = 8;
+		}
+	}
+}
