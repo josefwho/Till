8a5f27ad7ad34e8fb26bb3ddbdca4d754abce711
Felix Bohatsch <felix@brokenrul.es>
2015-01-09 14:44:27 +0100

    working on endscreen

--- Assets/EndScreen.cs
+++ Assets/EndScreen.cs
@@ -5,17 +5,24 @@ using System.Collections;
 public class EndScreen : MonoBehaviour {
 
 	private Text soldItemsRegular;
+	private TillStateMachine till;
 
 	// Use this for initialization
 	void Start () {
 		bool bla = false;
 
+		till = gameObject.FindGameObjectWithTag ("GameController").GetComponent<TillStateMachine> ();
 		soldItemsRegular = transform.Find("soldItemsRegular").GetComponent<Text> ();
 
 		string defaultText = soldItemsRegular.text;
-		addScoreToText(out defaultText, 
+		addScoreToText (out defaultText, till.countScannedObjects);
 	}
-	
+
+	void addScoreToText(out string text, int score )
+	{
+//		text.find
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
