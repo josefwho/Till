b9eed087745c4cb60942111e08979b4ed923ab40
Felix Bohatsch <felix@brokenrul.es>
2015-04-07 12:30:28 +0200

    baskettrigger recognizes an unscanned item

--- Assets/Scripts/GameLogic/BasketTrigger.cs
+++ Assets/Scripts/GameLogic/BasketTrigger.cs
@@ -2,25 +2,47 @@
 using System.Collections;
 
 public class BasketTrigger : ItemTrigger {
-	
-	// Use this for initialization
-	public virtual float getScore()
-	{
-		float score = 0;
 
-		for (int i = 0; i < objectsInside.Count; i++) 
+//	private TillStateMachine till;
+//	
+//	void Start()
+//	{
+//		base.Start ();
+//		
+//		till = GameObject.FindGameObjectWithTag ("GameController").GetComponent<TillStateMachine> ();
+//		
+//	}
+
+//	public override virtual float getScore()
+//	{
+//		float score = 0;
+//
+//		for (int i = 0; i < objectsInside.Count; i++) 
+//		{
+//			GameObject temp = objectsInside[i] as GameObject;
+//			if(temp.GetComponent<ItemStatus>().scanned == 1)
+//			{
+//				score += 1.0f;
+//			}
+//			if(temp.GetComponent<ItemStatus>().scanned > 1)
+//			{
+//				score -= 1.0f;
+//			}
+//		}
+//		return score;
+//	}
+
+	public override void OnTriggerEnter(Collider other)
+	{
+		base.OnTriggerEnter (other);
+		
+		if (other.gameObject.tag == "ShoppingItem")
 		{
-			GameObject temp = objectsInside[i] as GameObject;
-			if(temp.GetComponent<ItemStatus>().scanned == 1)
-			{
-				score += 1.0f;
-			}
-			if(temp.GetComponent<ItemStatus>().scanned > 1)
-			{
-				score -= 1.0f;
-			}
+			ItemStatus status = other.gameObject.GetComponent<ItemStatus>();
+
+			if(status && status.scanned == 0)
+				status.customer.onFreeItem(other.gameObject);
 		}
-		return score;
 	}
 }
 
