a89238dadfde7b6d964cec0c82f5bd2d2345050c
Felix Bohatsch <felix@brokenrul.es>
2015-04-07 14:45:56 +0200

    added Kassa-iOS level and a multi touchable conveyor button

---
+++ Assets/moveBeltButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class moveBeltButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	public ConveyorBelt conveyor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerDown(PointerEventData data)
+	{
+		if (!conveyor)
+			return;
+
+		conveyor.startMovingBelt ();
+	}
+
+	public void OnPointerUp(PointerEventData data)
+	{
+		if (!conveyor)
+			return;
+
+		conveyor.stopMovingBelt ();
+	}
+}
