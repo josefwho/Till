5e005fee9f426ffa63b8537f3f7be95807aba59b
Felix Bohatsch <felix@brokenrul.es>
2015-08-07 13:31:37 +0200

    started ticker

---
+++ Assets/PanLeft.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanLeft : MonoBehaviour {
+
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		float x = transform.position.x;
+//		x -= speed;
+//
+//		transform.position
+
+		transform.position -= new Vector3 (speed, 0, 0);
+	}
+}
