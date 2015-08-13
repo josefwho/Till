c2e1c3e22bd4265b808c21bf7f7718faf6f8f97d
Felix Bohatsch <felix@brokenrul.es>
2015-08-13 21:14:36 +0200

    started ticker text

--- Assets/PanLeft.cs
+++ Assets/PanLeft.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PanLeft : MonoBehaviour {
 
 	public float speed = 2;
+	public float tickerCount = 2;
+
+	float pixelsMoved = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -18,5 +21,13 @@ public class PanLeft : MonoBehaviour {
 //		transform.position
 
 		transform.position -= new Vector3 (speed, 0, 0);
+
+		if ((pixelsMoved += speed) > tickerCount * 328) {
+			pixelsMoved = 0;
+			transform.position += new Vector3(tickerCount * 328, 0, 0);
+		}
+
 	}
+
+
 }
