2341638ef49fcfb9d487354cb7ca54794b874d73
Felix Bohatsch <felix@brokenrul.es>
2015-08-15 11:18:26 +0200

    PanLeft now correctly restarts at right side of screen

--- Assets/PanLeft.cs
+++ Assets/PanLeft.cs
@@ -8,9 +8,13 @@ public class PanLeft : MonoBehaviour {
 
 	float pixelsMoved = 0;
 
+	bool pastLeftEdge = false;
+
+	RectTransform rectTransform;
+
 	// Use this for initialization
 	void Start () {
-	
+		rectTransform = (RectTransform)transform;
 	}
 	
 	// Update is called once per frame
@@ -22,9 +26,14 @@ public class PanLeft : MonoBehaviour {
 
 		transform.position -= new Vector3 (speed, 0, 0);
 
-		if ((pixelsMoved += speed) > tickerCount * 328) {
+		if (!pastLeftEdge && rectTransform.anchoredPosition.x < (-1 * (Screen.width + rectTransform.rect.width))) {
+				pastLeftEdge = true;
+		}
+
+		if (pastLeftEdge && (pixelsMoved += speed) > (tickerCount-1) * rectTransform.rect.width) {
 			pixelsMoved = 0;
-			transform.position += new Vector3(tickerCount * 328, 0, 0);
+			pastLeftEdge = false;
+			rectTransform.anchoredPosition = new Vector3(rectTransform.rect.width/2, 0, 0);
 		}
 
 	}
