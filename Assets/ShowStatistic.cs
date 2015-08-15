fd6a01b1192bd67620236abbc138a3dc4f567334
Felix Bohatsch <felix@brokenrul.es>
2015-08-15 12:11:44 +0200

    Started implementation of more statistics

---
+++ Assets/ShowStatistic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowStatistic : MonoBehaviour {
+
+	public enum Statistics { Fired, BelowMinimumWage, AboveMinimumWage, TotalItemsScanned, TotalShifts }
+	public Statistics show = Statistics.Fired;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
